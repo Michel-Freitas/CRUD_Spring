0d98ef0d8219934ab58432d3ed350bd6bd588cbe
Michel-Freitas <michelfreitas85@outlook.com>
2019-12-29 22:07:07 -0300

    Listar todos os usuários

--- src/main/java/br/com/crudspring/controller/UsuarioController.java
+++ src/main/java/br/com/crudspring/controller/UsuarioController.java
@@ -1,11 +1,15 @@
 package br.com.crudspring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.crudspring.model.Usuario;
 import br.com.crudspring.service.UsuarioService;
 
 @Controller
@@ -24,5 +28,15 @@ public class UsuarioController {
 	public String cadastrarUsuario(@RequestParam String nome, String login, String senha) {
 		userServi.cadastroUser(nome, login, senha);
 		return "redirect:/";
+	}
+	
+	@RequestMapping(path = "/listar")
+	public ModelAndView listarUsuarios() {
+		ModelAndView model = new ModelAndView("sistema/listar");
+		List<Usuario> listaUsuario = userServi.getListaUsuario();
+		
+		model.addObject("listUser", listaUsuario);
+		
+		return model;
 	}
 }
